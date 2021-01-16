c00113364dd4499c9f7d4bb64f5ea207337585c7
jamieminh <48439646+jamieminh@users.noreply.github.com>
2021-01-17 00:13:49 +0700

    add some methods to handle client related actions

--- src/java/dao/ClientDAO.java
+++ src/java/dao/ClientDAO.java
@@ -5,13 +5,41 @@
  */
 package dao;
 
-
+import model.pojo.Client;
+import model.dbHandler.DBBean;
+import java.sql.Connection;
 
 /**
  *
- * @author WIN 10
+ * @author Jamie
  */
-public class ClientDAO {
+public class ClientDAO extends DAO {
 
-    
+    public ClientDAO(Connection con) {
+        super(con);
+    }
+
+    private DBBean db = this.getDb();
+
+    public Client getClientData(String fullName) {
+        String query = "SELECT * FROM APP.CLIENTS WHERE cname='" + fullName + "'";
+        String[] data = db.getRecords(query)[0];
+
+        Client client = new Client();
+
+        client.setId(Integer.parseInt(data[0]));
+        client.setAddress(data[2]);
+        client.setType(data[3]);
+        client.setUsername(data[4]);
+
+        return client;
+    }
+
+    public boolean updateClient(String username, String fullname, String type, String address) {
+        String query = String.format("UPDATE APP.CLIENTS SET cname='%s', ctype='%s', caddress='%s' WHERE uname='%s'", fullname, type, address, username);
+        boolean res = db.executeUpdate(query);
+        
+        return res;
+    }
+
 }
